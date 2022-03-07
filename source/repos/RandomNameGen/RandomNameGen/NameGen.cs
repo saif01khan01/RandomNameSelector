0e6b1c2afbb4b5d89452474215ca82d908c2be5e
msaif <msaif.khan78641@gmail.com>
2022-03-07 23:39:34 +0000

    Added functionality (Add,Remove And list all name) - V02 Random name selector

--- source/repos/RandomNameGen/RandomNameGen/NameGen.cs
+++ source/repos/RandomNameGen/RandomNameGen/NameGen.cs
@@ -16,23 +16,73 @@ namespace RandomNameGen
         public void SelectName(string[] names, int count)
         {
             string[] arr = new string[count];
-            
+
 
             for (int i = 0; i < count; ++i)
             {
                 int maxValue = names.Count() - 1;
                 int a = RandomNumber.Between(0, maxValue);
-                
+
                 if (arr.Contains(names[a]))
                 {
                     //if duplicate is in array index minused by 1 so loops again until no duplicate
                     --i;
-                } else
+                }
+                else
                 {
                     arr[i] = names[a];
                     Console.WriteLine(names[a]);
                 }
 
+            }
+
+        }
+
+        /// <summary>
+        /// Adds name to text file 
+        /// 
+        /// @TODO - Replace file path with your own
+        /// </summary>
+        /// <param name="name"> name to be added </param>
+        public void AddName(string name)
+        {
+            File.AppendAllText(@"C:\Users\msaif\source\repos\RandomNameGen\RandomNameGen\names.txt", name);
+        }
+
+        /// <summary>
+        /// Removes name from text file
+        /// 
+        /// @TODO - Replace file path with your own
+        /// </summary>
+        /// <param name="removefrom"> Array of names which holds the name user would like to remove </param>
+        /// <param name="name"> Name user would like to remove </param>
+        public void RemoveName(string name)
+        {
+            string[] remove_from = File.ReadAllLines(@"C:\Users\msaif\source\repos\RandomNameGen\RandomNameGen\names.txt");
+
+            for(int i = remove_from.Count()-1; i>=0; i--)
+            {
+                if (remove_from[i].Contains(name))
+                {
+                    remove_from = remove_from.Where((source, index) => index != i).ToArray();
+                    File.WriteAllLines(@"C:\Users\msaif\source\repos\RandomNameGen\RandomNameGen\names.txt", remove_from);
+                }
+            }
+
+        }
+        /// <summary>
+        /// Lists all names 
+        /// 
+        /// @TODO - Replace file path with your own
+        /// </summary>
+        /// <param name="names"> Array of names to be outputted to the user </param>
+        public void ListNames()
+        {
+            string [] names = File.ReadAllLines(@"C:\Users\msaif\source\repos\RandomNameGen\RandomNameGen\names.txt");
+
+            foreach ( var name in names)
+            {
+                Console.WriteLine(name);
             }
 
         }
--- source/repos/RandomNameGen/RandomNameGen/Runner.cs
+++ source/repos/RandomNameGen/RandomNameGen/Runner.cs
@@ -13,24 +13,60 @@ namespace RandomNameGen
         {
             NameGen test = new NameGen();
 
-            //Converts each line from text file to array element
-            string[] names = File.ReadAllLines(@"C:\Users\msaif\source\repos\RandomNameGen\RandomNameGen\names.txt");
+            Console.WriteLine("Choose one of the following operations (Enter the letter associated with the operation)" + Environment.NewLine
+                + "A. Select Random Names" + Environment.NewLine
+                + "B. Add name to list" + Environment.NewLine
+                + "C. Remove name from list" + Environment.NewLine
+                + "D. List all names" + Environment.NewLine);
 
-            Console.WriteLine("How many names would you like to select");
-            
-            string temp;
-            temp = Console.ReadLine();
-            int count = Int32.Parse(temp);
+            string operation = Console.ReadLine();
 
-            //if user chooses number of names larger than list will fail
-            if (count > names.Count())
+            operation.ToUpper();
+
+            // Validation of user input
+            switch (operation)
             {
-                Console.WriteLine("The Number of names you would like to pick is greater than the number of names in the list try again");
+                case "A":
+                    //Converts each line from text file to array element
+                    string[] names = File.ReadAllLines(@"C:\Users\msaif\source\repos\RandomNameGen\RandomNameGen\names.txt");
+
+                    Console.WriteLine("How many names would you like to select");
+
+                    string temp;
+                    temp = Console.ReadLine();
+                    int count = Int32.Parse(temp);
+
+                    //if user chooses number of names larger than list will fail
+                    if (count > names.Count())
+                    {
+                        Console.WriteLine("The Number of names you would like to pick is greater than the number of names in the list try again");
+                    }
+                    else
+                    {
+                        test.SelectName(names, count);
+                    }
+                    break;
+
+                case "B":
+                    Console.WriteLine("Please enter the name you would like to add");
+                    string name_to_add = Console.ReadLine();
+                    test.AddName(name_to_add);
+                    break;
+
+                case "C":
+                    Console.WriteLine("Please enter the name you would like to remove");
+                    string name_to_remove = Console.ReadLine();
+
+                    test.RemoveName(name_to_remove);
+                    break;
+
+                case "D":
+                    test.ListNames();
+                    break;
             }
-            else
-            {
-                test.SelectName(names, count);
-            }
+
+
+
 
             
 
